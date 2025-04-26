942cec479a25fd52cb5a09820b084cc838d1c323
ChristianCamiloPG <christiancamilopulidog@gmail.com>
2025-04-26 00:37:54 +0000

    caMBIO7

--- api_coingecko_app/js/datos.js
+++ api_coingecko_app/js/datos.js
@@ -3,7 +3,7 @@ function mostrarAcerca() {
   app.innerHTML = `
     <h1>Acerca de</h1>
     <p>App de criptomonedas usando CoinGecko API.</p>
-    <p>Desarrollador: <strong>Tu Nombre</strong></p>
+    <p>Desarrollador: <strong>Christian pulido</strong></p>
     <p>Versión: <strong>1.0.0</strong></p>
     <img src="img/coingecko.png" alt="Logo" width="100">
   `;
